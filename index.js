9d58ad640552a00bf16d57ffdd8500058ed2ad84
kailastr <mailtokailastr@gmail.com>
2023-10-16 22:54:06 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const CatImageGenerator = () => {
+
+    const CatImages = [
+        "https://img.freepik.com/free-photo/cute-domestic-kitten-sitting-leaf-generative-ai_188544-8316.jpg",
+        "https://img.freepik.com/premium-photo/painting-cat-with-green-eyes-generative-ai_922357-9651.jpg",
+        "https://img.freepik.com/premium-photo/cute-little-cat-playing-forest_432372-183.jpg",
+        "https://img.freepik.com/premium-photo/close-up-gray-kitten-natureai_725102-3022.jpg",
+        "https://img.freepik.com/premium-photo/there-is-small-kitten-sitting-log-dark-generative-ai_958108-6670.jpg",
+    ]
+
+    var randomImage = Math.floor(Math.random() * 5);
+
+}
